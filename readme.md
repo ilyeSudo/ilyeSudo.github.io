c9ec16612249526cee9b7aa1c687d0e33cb696fc
ilyeSudo <houdjedje.ilyes@gmail.com>
2020-03-06 17:37:53 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+This was made with the excellent `Gatsby.js` and is hosted on `Netlify`. Thank you [Preston Richey](https://prestonrichey.com) for the template!
---
+++ src/pages/projects.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import Layout from './../components/Layout';
+
+const ProjectsPageInner = props => {
+  return (
+    <div>
+      <h3>wip...</h3>
+    </div>
+  );
+};
+
+const ProjectsPage = props => {
+  return (
+    <Layout>
+      <ProjectsPageInner {...props} />
+    </Layout>
+  );
+};
+
+export default ProjectsPage;
+
+// export const query = graphql`
+//   query {
+//     allMdx(
+//       filter: {
+//         frontmatter: { publish: { eq: true } }
+//         fields: { type: { eq: "project" } }
+//       }
+//       sort: { fields: [frontmatter___date], order: DESC }
+//     ) {
+//       edges {
+//         node {
+//           frontmatter {
+//             title
+//             date
+//             lede
+//           }
+//           fields {
+//             slug
+//             projectType
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
