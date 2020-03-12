b41205e4c3ba17be3288f3b51e817dad4afbecfd
ilyeSudo <houdjedje.ilyes@gmail.com>
2020-03-12 09:45:48 +0100

    Update resume link

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -5,18 +5,18 @@ import Layout from './../components/Layout';
 const ProjectsPageInner = props => {
   return (
     <object
-      data="https://drive.google.com/file/d/1hwtPpDsRaD-6qVGE53lf615NhehwhaTF/preview"
+      data="https://drive.google.com/file/d/14fHrWV96NCbpsHyh6E6HZd0s6hLdnvEj/preview"
       width="100%"
       height="100%"
     >
       <embed
-        src="https://drive.google.com/file/d/1hwtPpDsRaD-6qVGE53lf615NhehwhaTF/preview"
+        src="https://drive.google.com/file/d/14fHrWV96NCbpsHyh6E6HZd0s6hLdnvEj/preview"
         width="100%"
         height="100%"
       />
       <p>
         This browser does not support PDFs. Please download the PDF to view it:
-        <a href="https://drive.google.com/file/d/1hwtPpDsRaD-6qVGE53lf615NhehwhaTF/preview">
+        <a href="https://drive.google.com/file/d/14fHrWV96NCbpsHyh6E6HZd0s6hLdnvEj/preview">
           View the PDF
         </a>
         .
